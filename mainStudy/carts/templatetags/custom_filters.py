2dafc58abfccc57cd50a164265acf1c536ae3962
King <russki-pusi@mail.ru>
2025-05-23 19:24:55 +0300

    CART!

---
+++ mainStudy/carts/templatetags/custom_filters.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def multiply(value, arg):
+    """Умножает значение на аргумент."""
+    try:
+        return value * arg
+    except (TypeError, ValueError):
+        return ''
